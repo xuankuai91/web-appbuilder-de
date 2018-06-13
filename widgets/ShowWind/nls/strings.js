88ab4fd25ccd095f401fe1c542f3715a7032b74e
xuankuai91 <xuankuai91@gmail.com>
2018-06-13 10:29:24 -0500

    Create strings.js

---
+++ widgets/ShowWind/nls/strings.js
@@ -0,0 +1,16 @@
+define({
+    root:({
+        _widgetLabel: "Show Wind", // Define the name displayed in the widget list
+
+        // Define the texts in the widget panel
+        windDirection: "Wind direction",
+        degreesFrom: "Degrees from: ",
+        degreesFromNote: "(0 - 359)",
+        setDirection: "Set direction",
+
+        windSpeed: "Wind speed",
+        speed: "Speed: ",
+        speedNote: "(mph)",
+        setSpeed: "Set speed"
+    }) 
+});
